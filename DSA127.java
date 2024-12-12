22a6b9d2eb3b052209ffe042d17881050bfce564
RonaldoN71 <ronaldonandeibam7@gmail.com>
2024-12-12 14:08:48 +0530

    change in the list entry

--- DSA127.java
+++ DSA127.java
@@ -24,7 +24,7 @@ public class DSA127 {
     }
     public static void main(String[]args){
         Node head = new Node(1);
-        head.next = new Node(2);
+        head.next = new Node(6);
         head.next.next = new Node(3);
         head.next.next.next = new Node(4);
 
